662e6bf02329292240eeed3577482942cf50a3e1
Ryan <ryanshaunsmith@gmail.com>
2025-06-08 14:45:00 +0100

    add an api GET endpoint for tasks in the server and useFetch it from the root page

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,8 @@
-<template><h1>Hello Nuxt not Next</h1></template>
+<script setup lang="ts">
+const { data: tasks, error, status } = await useFetch("/api/tasks");
+</script>
+
+<template>
+  <h1>Hello Nuxt not Next</h1>
+  <article v-for="task in tasks" :key="task.id">{{ task.title }}</article>
+</template>
---
+++ server/api/tasks.get.ts
@@ -0,0 +1,19 @@
+export default defineEventHandler(() => {
+  return [
+    {
+      id: 1,
+      title: "Learn Nuxt",
+      done: false,
+    },
+    {
+      id: 2,
+      title: "Learn Vue",
+      done: false,
+    },
+    {
+      id: 3,
+      title: "Put it all together",
+      done: false,
+    },
+  ];
+});
