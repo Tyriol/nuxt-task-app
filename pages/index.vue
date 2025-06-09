357d2b3a8cd8b48bc5513646ef17a45e25b8db24
Ryan <ryanshaunsmith@gmail.com>
2025-06-09 10:57:51 +0100

    add drizzle-zod schema for creating a tasks table and validating it

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,6 @@ const { data: tasks, error, status } = await useFetch("/api/tasks");
 </script>
 
 <template>
-  <h1>Hello Nuxt not Next</h1>
+  <h1>Your tasks:</h1>
   <article v-for="task in tasks" :key="task.id">{{ task.title }}</article>
 </template>
