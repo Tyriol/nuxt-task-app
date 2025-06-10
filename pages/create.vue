b18eb2b7c7b211e7de75b226be48c57b48fd8c32
Ryan <ryanshaunsmith@gmail.com>
2025-06-10 08:35:16 +0100

    add endpoint to post new task and call it from the frontend

--- pages/create.vue
+++ pages/create.vue
@@ -1,3 +1,57 @@
+<script setup lang="ts">
+import { FetchError } from "ofetch";
+
+const errorMessage = ref("");
+const loading = ref(false);
+const taskName = ref("");
+
+async function onSubmit() {
+  if (!taskName.value.trim()) {
+    errorMessage.value = "Task cannot be blank";
+    return;
+  }
+  try {
+    loading.value = true;
+    errorMessage.value = "";
+    const result = await $fetch("/api/tasks", {
+      method: "POST",
+      body: {
+        title: taskName.value,
+      },
+    });
+    navigateTo({
+      name: "tasks-id",
+      params: {
+        id: result.id,
+      },
+    });
+  } catch (e) {
+    const error = e as FetchError;
+    errorMessage.value = error.statusMessage || "An unexpected error has occurred";
+  } finally {
+    loading.value = false;
+  }
+}
+</script>
+
 <template>
-  <h1>Create a task!</h1>
+  <div class="create-task-container">
+    <form @submit.prevent="onSubmit">
+      <label>
+        Task:
+        <input v-model="taskName" name="title" />
+      </label>
+      <div class="button-container"><button>Create</button></div>
+    </form>
+    <article v-if="loading" aria-busy="true" />
+    <article v-else-if="errorMessage" class="error">{{ errorMessage }}</article>
+  </div>
 </template>
+
+<style scoped>
+.create-task-container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
---
+++ server/api/tasks.post.ts
@@ -0,0 +1,20 @@
+import db from "~/lib/db";
+import { InsertTasksSchema, tasks } from "~/lib/db/schema";
+
+export default defineEventHandler(async (event) => {
+  const result = await readValidatedBody(event, InsertTasksSchema.safeParse);
+
+  if (!result.success) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 422,
+        statusMessage: "Invalid Task",
+      })
+    );
+  }
+
+  const [created] = await db.insert(tasks).values(result.data).returning();
+
+  return created;
+});
