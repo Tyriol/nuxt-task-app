3042677f8e297c62e7af8cbdfe30ed1fe64eeab5
Ryan <ryanshaunsmith@gmail.com>
2025-06-10 16:12:14 +0100

    add ability to toggle done status from the view task page

--- pages/tasks/[id].vue
+++ pages/tasks/[id].vue
@@ -8,12 +8,39 @@ const {
 } = await useFetch(`/api/tasks/${route.params.id}`, {
   lazy: true,
 });
+
+async function toggleDone() {
+  if (!task.value) return;
+
+  try {
+    const result = await $fetch(`/api/tasks/${task.value.id}`, {
+      method: "PATCH",
+      body: {
+        done: !task.value.done,
+      },
+    });
+
+    if (!result) throw Error;
+    console.log(result);
+    task.value.done = result[0].done;
+  } catch (error) {
+    console.error(error);
+  }
+}
 </script>
 
 <template>
   <article v-if="status === 'pending'" aria-busy="true" />
   <article class="error" v-else-if="error">{{ error.statusMessage }}</article>
   <div v-else-if="task">
-    <article>{{ task.title }}</article>
+    <article>
+      <label>
+        <input @input="toggleDone" type="checkbox" :checked="task.done" />
+      </label>
+      <span :class="{ done: task.done }">{{ task.title }}</span>
+    </article>
+    <div class="button-container">
+      <button>Remove</button>
+    </div>
   </div>
 </template>
