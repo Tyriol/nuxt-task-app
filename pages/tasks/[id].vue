586c2cdb966d5e95fcd1622a1b9800af1bd747c3
Ryan <ryanshaunsmith@gmail.com>
2025-06-10 08:36:55 +0100

    add dynamic route and endpoint for a single task

---
+++ pages/tasks/[id].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const {
+  data: task,
+  error,
+  status,
+} = await useFetch(`/api/tasks/${route.params.id}`, {
+  lazy: true,
+});
+</script>
+
+<template>
+  <article v-if="status === 'pending'" aria-busy="true" />
+  <article class="error" v-else-if="error">{{ error.statusMessage }}</article>
+  <div v-else-if="task">
+    <article>{{ task.title }}</article>
+  </div>
+</template>
---
+++ server/api/tasks/[id].get.ts
@@ -0,0 +1,38 @@
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import db from "~/lib/db";
+import { tasks } from "~/lib/db/schema";
+
+const IdParamSchema = z.object({
+  id: z.coerce.number(),
+});
+
+export default defineEventHandler(async (event) => {
+  const result = await getValidatedRouterParams(event, IdParamSchema.safeParse);
+
+  if (!result.success) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 422,
+        statusMessage: "Invalid id",
+      })
+    );
+  }
+
+  const task = await db.query.tasks.findFirst({
+    where: eq(tasks.id, result.data.id),
+  });
+
+  if (!task) {
+    return sendError(
+      event,
+      createError({
+        statusCode: 404,
+        statusMessage: "Task not found",
+      })
+    );
+  }
+
+  return task;
+});
