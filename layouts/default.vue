25f5b000c889a0bbe62f22e00fcd6406949f464d
Ryan <ryanshaunsmith@gmail.com>
2025-06-10 13:23:13 +0100

    add a link to get to the individual task and update button container styling if there is more than one button

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,7 @@
 .button-container {
   display: flex;
   justify-content: end;
+  gap: 1rem;
 }
 
 .error {
--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,11 @@ const {
   <article class="error" v-else-if="error">{{ error.statusMessage }}</article>
   <div v-else>
     <article v-if="tasks?.length === 0">Add a task to get started</article>
-    <article v-else v-for="task in tasks" :key="task.id">{{ task.title }}</article>
+    <article v-else v-for="task in tasks" :key="task.id">
+      {{ task.title }}
+      <div class="button-container">
+        <NuxtLink role="button" :to="{ name: 'tasks-id', params: { id: task.id } }">View</NuxtLink>
+      </div>
+    </article>
   </div>
 </template>
