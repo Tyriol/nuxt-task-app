b3211375a6700b34a332c7b8b2855ac23e72a57c
Ryan <ryanshaunsmith@gmail.com>
2025-06-13 14:58:08 +0100

    add page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
   css: ["@picocss/pico"],
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   eslint: {
     config: {
       stylistic: {
